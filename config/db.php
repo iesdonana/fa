608db61443b7d5f8a01c056dde33317fc51c09f8
Ricardo Pérez <ricpelo@gmail.com>
2016-12-16 15:46:28 +0100

    Desactiva el caché del esquema de BD

--- config/db.php
+++ config/db.php
@@ -6,11 +6,4 @@ return [
     'username' => 'fa',
     'password' => 'fa',
     'charset' => 'utf8',
-    'enableSchemaCache' => true,
-
-    // Duration of schema cache.
-    'schemaCacheDuration' => 3600,
-
-    // Name of the cache component used to store schema information
-    'schemaCache' => 'cache',
 ];
